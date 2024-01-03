59fd6139d76f706807e24e0eaa3b9c22847a0ece
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2024-01-03 02:49:25 -0300

    add filterProductByCategory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,43 @@
-'use client'
+"use client"
 import { CardProduct } from "@/components/Cardproduct";
 import { Footer } from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Section_title from "@/components/Section_title";
 import { products } from "@/data/productstList";
-import { useState } from "react";
-
+import { ProductType } from "@/types/product";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [catSelected, setCatSelected] = useState("Perfumaria");
+  const [filteredProducts, setFilteredProducts] = useState<ProductType[]>([]
+  );
+
+  const filterProductByCategory = useCallback((categorySelected: string) => {
+    const result = products.filter((product) => product.category === categorySelected);
+    setFilteredProducts(result);
+    setCatSelected(categorySelected);
+    return result
+  }, []);
+
+  useEffect(() => {
+    filterProductByCategory("Perfumaria");
+  }, [filterProductByCategory]);
+
+
   return (
     <>
       <Header />
-      <Navbar toggleCategory={setCatSelected}/>
+      <Navbar toggleCategory={filterProductByCategory} />
       <main className="py-4 z-0">
-        <Section_title title={catSelected}/>
+        <Section_title title={catSelected} />
         <div className="w-full flex flex-wrap  justify-around gap-4 pt-4 px-2 ">
-          {products.map((product)=>(
-            <CardProduct product={product} key={product.id}/>
+          {filteredProducts.map((product) => (
+            <CardProduct product={product} key={product.id} />
           ))}
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
 'use client'
+import { ProductType } from "@/types/product";
 import { categoriesList } from "@/util/categories";
-import { Dispatch, SetStateAction } from "react";
 
-interface InavbarProp{
-    toggleCategory: Dispatch<SetStateAction<string>>;
+interface InavbarProp {
+    toggleCategory: (categorySelected: string) => ProductType[];
 }
-export default function Navbar(props:InavbarProp){
-    return(
-        <ul className="w-full min-w-full overflow-x-auto  flex justify-start items-center py-2 px-2 gap-4">
-         {categoriesList.map((item)=>(
-            <li key={item} className="font-semibold text-sm text-orange-800 hover:text-orange-600 flex-1 p-2" onClick={()=>props.toggleCategory(item)}>{item}</li>
-         ))}
-        </ul>
+export default function Navbar(props: InavbarProp) {
+    return (
+        <nav className="w-full min-w-full">
+            <ul className="w-full min-w-full overflow-x-auto  flex justify-start items-center py-2 px-2 gap-4">
+                {categoriesList.map((item) => (
+                    <li key={item} className="font-semibold text-sm text-orange-800 hover:text-orange-600 flex-1 p-2" onClick={() => props.toggleCategory(item)}>{item}</li>
+                ))}
+            </ul>
+        </nav>
+
     )
 }
